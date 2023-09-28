bb2740f529f37aa8c4567c110df3ea1a9e4fb1aa
Erik Barcella <agenciaebdigital@gmail.com>
2023-09-28 10:03:35 -0300

    alteracoes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const connectDatabase = require('./database/getConnection')
 const userRoutes = require('./routes/userRoutes');
 const methodOverride = require('method-override');
 require('dotenv').config();
+const fs = require('fs');
+const dotenv = require('dotenv-safe');
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +14,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(userRoutes);
 
+if (fs.existsSync('.env.example')) {
+    dotenv.config({
+      example: '.env.example',
+    });
+  } else {
+    console.warn('O arquivo .env.example não foi encontrado. Ignorando.');
+  }
 
 app.get('*', (req,res)=>{
     res.send('Server is running v1.0.0')
