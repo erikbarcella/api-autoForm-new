c97b3c14d37c1e3b2c308c1edd774d30634ed776
Erik Barcella <agenciaebdigital@gmail.com>
2023-10-10 19:48:41 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const passport = require("../middleware/authStrategies"); // Importar o módulo que exportamos anteriormente
+const passport = require("../middleware/authStrategies"); 
 const User = require("../models/user");
 const expressSession = require("express-session");
 const isAdmin = require("../middleware/checkAdmin")
@@ -17,7 +17,6 @@ routes.use(passport.initialize());
 routes.use(passport.session());
 
 routes.post('/login',(req,res,next)=>{
-    //console.log(req.body)
     passport.authenticate("local", (err, user, info) => {
         if (err) throw err;
         if (!user){
@@ -87,54 +86,67 @@ routes.get('/users', passport.authenticate('jwt', { session: false }), async (re
   });
 
   // Rota para alterar a senha de um usuário específico
-routes.put('/users/:id/senha', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    // Verifique se o usuário é um administrador ou se está alterando sua própria senha
-    if (!req.user.isAdmin && req.user._id.toString() !== req.params.id) {
-      return res.status(403).json({ message: 'Você não tem permissão para alterar a senha deste usuário' });
+  routes.put('/users/:id/senha', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+      if (!req.user.isAdmin && req.user._id.toString() !== req.params.id) {
+        return res.status(403).json({ message: 'Você não tem permissão para alterar a senha deste usuário' });
+      }
+      if (req.body.newPassword.length < 6) {
+        return res.status(400).json({ message: 'A nova senha deve ter pelo menos 6 caracteres' });
+      }
+      
+      const user = await User.findById(req.params.id);
+      
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+      // Atualize o hash e salt da senha do usuário para a nova senha fornecida
+      user.setPassword(req.body.newPassword, async () => {
+        try {
+          await user.save();
+          return res.status(200).json({ message: 'Senha do usuário atualizada com sucesso' });
+        } catch (err) {
+          console.error(err);
+          return res.status(500).json({ message: 'Erro ao salvar a nova senha' });
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Erro ao alterar a senha' });
     }
-
-    // Implemente a lógica para alterar a senha do usuário com base no ID
-    // ...
-
-    return res.status(200).json({ message: 'Senha alterada com sucesso' });
-  } catch (err) {
-    return res.status(500).json({ message: 'Erro ao alterar a senha' });
-  }
-});
-
+  });
+  
 
 // Rota para deletar um usuário específico
 routes.delete('/users/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  console.log(req.params.id)
   try {
-    // Verifique se o usuário é um administrador ou se está excluindo sua própria conta
     if (!req.user.isAdmin && req.user._id.toString() !== req.params.id) {
       return res.status(403).json({ message: 'Você não tem permissão para excluir este usuário' });
     }
-
-    // Implemente a lógica para excluir o usuário com base no ID
-    // ...
-
-    return res.status(200).json({ message: 'Usuário excluído com sucesso' });
+    if(req.user.isAdmin){
+      let res = await User.deleteOne({"_id": req.params.id});
+      if(res.deletedCount === 0)return res.status(404).json({ message: 'Usuário não encontrado' });
+      return res.status(200).json({ message: 'Usuário excluído com sucesso' });
+    }
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ message: 'Erro ao excluir o usuário' });
   }
 });
 
-
 // Rota para autorizar um usuário específico
 routes.put('/users/:id/autorizar', passport.authenticate('jwt', { session: false }), async (req, res) => {
   try {
-    // Verifique se o usuário é um administrador
     if (!req.user.isAdmin) {
       return res.status(403).json({ message: 'Você não tem permissão para autorizar usuários' });
     }
-
-    // Implemente a lógica para autorizar o usuário com base no ID
-    // ...
-
-    return res.status(200).json({ message: 'Usuário autorizado com sucesso' });
+    if(req.user.isAdmin){
+      await User.updateOne({_id: req.params.id}, {$set: {isApproved: true}})
+      return res.status(200).json({ message: `Usuário autorizado com sucesso` });
+    } 
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ message: 'Erro ao autorizar o usuário' });
   }
 });
